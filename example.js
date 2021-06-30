ac28fae9ca7864e06638043c09ef3418691b00ab
Christopher Tarquini <code@tarq.io>
2021-06-30 10:56:41 -0400

    eap program examples, seperated wrapper to example.js

---
+++ example.js
@@ -0,0 +1,187 @@
+const EventEmitter = require('events');
+const { createHmac } = require('crypto')
+
+const LaunchDarkly = require('launchdarkly-node-server-sdk');
+
+const faker = require('faker')
+const pkg = require('./package.json')
+const micromatch = require('micromatch')
+const {v4: uuid} = require('uuid')
+
+
+
+let ldClient = null;
+
+
+/**
+ * Returns an initalized LaunchDarkly Client
+ * @returns {LaunchDarkly.LDClient} 
+ */
+function initializeLaunchDarklyClient() {
+    return  LaunchDarkly.init(process.env.LD_SDK_KEY, {
+            privateAttributeNames: ['Date of Birth']
+        })
+}
+
+/**
+ * Returns an initialized LaunchDarkly Client as a singleton.
+ * @returns {LaunchDarkly.LDClient}
+ */
+function getLDClient() {
+    if (ldClient === null) {
+        ldClient = initializeLaunchDarklyClient()
+        setupEventListeners(ldClient)
+    }
+    return ldClient
+}
+
+
+/**
+ * Wrapper around variation calls
+ * @param {LaunchDarkly.LDUser} user 
+ * @param {string} flag 
+ * @returns {any}
+ */
+ function variation(flag,user) {
+    const ld = getLDClient()
+    return ld.variation(flag, user, getFallbackValue(flag))
+}
+
+/**
+ * Returns a randomly generated user (for demo purposes)
+ * Takes in a set of properties to merge into the random user
+ * @param {LaunchDarkly.LDUser} 
+ * @returns {LaunchDarkly.LDUser}
+ */
+function getUser(properties) {
+
+    const [firstName, lastName] = [faker.name.firstName(), faker.name.lastName()]
+    const username = faker.internet.userName(firstName, lastName)
+
+    const groups = ['admin', 'user', 'editor', 'reviewer', 'author']
+    const datacenters = ['us-east-1', 'us-east-2', 'eu-west-1', 'eu-west-2']
+
+    const anonymous = faker.datatype.number({min: 1, max: 100}) < 60
+    const sessionIdentifer = uuid()
+
+    const user = {
+      // `key` is a unique, consistent identifier used for rollouts
+      // use a Session Identifer for unauthenticated users
+      // use User Identifer for authenticated users
+      key: anonymous ? sessionIdentifer : getUserIdentifer(username),
+      email: faker.internet.email(username),
+      username,
+      name: `${firstName} ${lastName}`,
+      firstName,lastName,
+      ip: faker.internet.ip(),
+      anonymous: anonymous,
+      custom: {
+        'Session': uuid(),
+        'Date of Birth': faker.date.past(50, new Date("Sat Sep 20 1992 21:35:02 GMT+0200 (CEST)")),
+        'Tenant': `${faker.lorem.word()} ${faker.company.companySuffix()}`,
+        'Country': faker.address.countryCode(),
+        'Groups': faker.random.arrayElements(groups, faker.datatype.number({min: 1, max: 3})),
+        'Service Version': pkg.version,
+        'Service Name': pkg.name,
+        'Service Hostname': 'some-server.example.com',
+        // anonymous users probably wouldn't have any opt-ins..
+        'EAP Opt-ins': anonymous ? [] : faker.random.arrayElements(Array.from(getAllEarlyAccessPrograms()), faker.datatype.number({min: 0, max: 3}))
+      }
+    }
+    // merge properties
+    const {custom} = user;
+    return Object.assign(user, properties || {}, {custom: Object.assign(custom, properties && properties.custom || {})})
+}
+
+
+
+
+
+// listen of all EAPs discovered via flag naming convention
+const earlyAccessPrograms = new Set();
+
+/**
+ * Get all early access program keys
+ * @returns {Set<String>}
+ */
+function getAllEarlyAccessPrograms() {
+    return earlyAccessPrograms
+}
+
+
+/**
+ * Gets a list of early access programs a user is allowed to opt-in to
+ * @param {*} user 
+ * @returns 
+ */
+async function getAvailableEarlyAccessPrograms(user) {
+    const eaps = getAllEarlyAccessPrograms()
+    return Promise.all(
+        Array.from(eaps)
+        .map(async (v) => [await variation(`allow-early-access-program-${v}`,user), v])
+    )
+    .then(results => 
+        results
+            .filter(([allow]) => allow)
+            .map(([_,v]) => v)
+    )
+}
+
+function setupEventListeners(client) {
+    // wrapper to support glob-style subscriptions
+    let flagUpdateEventEmitter = new EventEmitter()
+    // update the list of EAP programs based on our naming convention
+    flagUpdateEventEmitter.on('allow-early-access-program-*',
+     ({key}) => 
+        earlyAccessPrograms.add(
+            key.split('allow-early-access-program-', 2).pop()
+    ))
+
+    client.on('update', (flag) => {
+        flagUpdateEventEmitter.eventNames()
+            .filter(event => micromatch.isMatch(flag.key, event))
+            .forEach(event => flagUpdateEventEmitter.emit(event, flag))
+    })
+}
+
+
+// example map of flag keys to fallback values
+const fallbacks = new Map(Object.entries({
+    'release-widget': false
+}))
+
+/**
+ * Centralizes fallback management for features. Can be used to load them from a file etc
+ * @param {string} flag
+ */
+function getFallbackValue(flag) {
+    if (!fallbacks.has(flag) && flag.startsWith('allow-early-access-program')) {
+        return false
+    }
+    return fallbacks.get(flag)
+}
+
+function setFallbackValue(flag, value) {
+    return fallbacks.set(flag, value)
+}
+
+/**
+ * Returns the user identifer for a given username. Uses an HMAC for the demo purposes. 
+ * @param {string} username 
+ */
+function getUserIdentifer(username) {
+    const secret = 'amiably-tundra-pluto-stargaze'
+    return createHmac('sha256', secret).update(username).digest('hex')
+}
+
+
+module.exports = {
+    initializeLaunchDarklyClient,
+    getUser,
+    getLDClient,
+    getUserIdentifer,
+    variation,
+    getAllEarlyAccessPrograms,
+    getAvailableEarlyAccessPrograms
+
+}
--- index.js
+++ index.js
@@ -1,44 +1,16 @@
-const LaunchDarkly = require('launchdarkly-node-server-sdk');
-const faker = require('faker')
-const { LEVEL, MESSAGE } = require('triple-beam');
-const winston = require('winston')
 const chdar = require('chokidar') 
 const {format} = require('util')
-const {v4 : uuid} = require('uuid')
 const blessed = require('blessed')
 const contrib = require('blessed-contrib')
-
+const example = require('./example')
 const {readFile} = require('fs').promises
-const pkg = require('./package.json');
-const { toNamespacedPath } = require('path');
-const hostname = require('os').hostname()
+const {v4: uuid} = require('uuid')
+
 const GRID_COLS = 24
 const GRID_ROWS = 80
 const USER_COUNT = GRID_COLS * GRID_ROWS
-const Transport = require('winston-transport');
-const { DH_CHECK_P_NOT_PRIME } = require('constants');
 
 
-class CustomTransport extends Transport {
-  constructor(opts) {
-    super(opts);
-    this._fn = opts.fn
-    //
-    // Consume any custom options here. e.g.:
-    // - Connection information for databases
-    // - Authentication information for APIs (e.g. loggly, papertrail,
-    //   logentries, etc.).
-    //
-  }
-
-  log(info, callback) {
-    
-    this._fn(`${(new Date()).toISOString()}} [${info[LEVEL]}]: ${info.message }`)
-    // Perform the writing to the remote service
-
-    callback();
-  }
-};
 
 
 let exampleUsers = [{
@@ -53,41 +25,20 @@ return watcher = chdar.watch('users.json', {
 });
 }
 
-function generateUser() {
-  const [firstName, lastName] = [faker.name.firstName(), faker.name.lastName()]
-  const username = faker.internet.userName(firstName, lastName)
-  const groups = ['admin', 'user', 'editor', 'reviewer', 'author']
-  return {
-    key: uuid(),
-    email: faker.internet.email(username),
-    username,
-    name: `${firstName} ${lastName}`,
-    firstName,lastName,
-    ip: faker.internet.ip(),
-    custom: {
-      dob: faker.date.past(50, new Date("Sat Sep 20 1992 21:35:02 GMT+0200 (CEST)")),
-      organization: `${faker.lorem.word()} ${faker.company.companySuffix()}`,
-      country: faker.address.countryCode(),
-      groups: faker.random.arrayElements(groups, faker.datatype.number({min: 1, max: 3})),
-      app_version: pkg.version,
-      app_name: pkg.name,
-      app_hostname: hostname
-    }
-  }
-}
 
 function generateUsers() {
   const users = []
   while (users.length < USER_COUNT) {
-    users.push(generateUser())
+    users.push(example.getUser())
   }
   return users
 }
 
 const randomUsers = generateUsers()
+
 async function refreshUsers() {
   const data = await readFile('./users.json', {encoding: 'utf8' })
-  exampleUsers = JSON.parse(data)
+  exampleUsers = JSON.parse(data).map(example.getUser)
   return exampleUsers.concat(randomUsers.slice(0, randomUsers.length - exampleUsers.length))
 }
 
@@ -97,7 +48,7 @@ const screen = blessed.screen({
 const userBox = blessed.box({
   
   height: '50%',
-  width: '50%',
+  width: '100%',
   top: 0,
   left: 0,
 
@@ -143,7 +94,7 @@ const rolloutBox = blessed.box({
   height: '50%',
   width: '100%',
   border: 'line',
-  label: "Rollout",
+  label: "Rollout: release-widget",
   sendFocus: true,
   scrollable: true
   
@@ -153,44 +104,48 @@ const rolloutDisplay = blessed.text({
   tags: true,
 })
 rolloutDisplay.enableMouse()
-screen.append(loggerBox)
+//screen.append(loggerBox)
 screen.append(userBox)
 screen.append(rolloutBox)
 screen.enableMouse()
 rolloutBox.focus()
 
 const noop = (info, callback) => { 
-  logger.log(info)
+  //logger.log(info)
   if (callback) {
     callback()
   }
   screen.render()
 
 }
-const transport = new CustomTransport({fn: noop})
-const ldLogger = winston.createLogger({
-  transports: [transport]
-})
 
-const ld = LaunchDarkly.init(process.env.SDK_KEY, {
-  logger: ldLogger,
-  sendEvents: true
-});
-const variation = ld.variation.bind(ld)
+const ld = example.getLDClient()
+const variation = example.variation
+
 async function render() {
   const users = await refreshUsers()
   const dummyUser = {key: "anonymous", anonymous: true}
   const flags = (await ld.allFlagsState(dummyUser)).allValues()
-  const flagKeys = Object.keys(flags);
+  const flagKeys = Object.keys(flags).filter(v => !v.startsWith('allow-early-access-program-'));
   const table = [['Flag'].concat(exampleUsers.map((user => user.name)))]
-  const rows = (await Promise.all(flagKeys.map(key => Promise.all([key].concat(exampleUsers.map(user => ld.variation(key, user)
- )))))).map(([first, ...rest]) => [first, ...rest.map(v => v.toString())])
+  const rows = (await Promise.all(
+    flagKeys.map(key => 
+      Promise.all(
+        [key].concat(
+          exampleUsers.map(user => ld.variation(key, user))
+      ))
+      )))
+    .map(
+      ([first, ...rest]) => [first, ...rest.map(v => v.toString())]
+    )
   
   table.push(...rows)
+  const eaps = await Promise.all(exampleUsers.map(user => example.getAvailableEarlyAccessPrograms(user)))
+  table.push(['available eaps'].concat(eaps.map(v => v.join(','))))
   userTable.setData(table)
 
   screen.render()
-  const evals = await Promise.all(users.map(user => Promise.all([user, ld.variation('release-widget', user, false)])))
+  const evals = await Promise.all(users.map(user => Promise.all([user, variation('release-widget', user)])))
   
   rolloutDisplay.setContent(evals.map(([user, result]) =>  `${result ? '{green-fg}' : '{blue-fg}'}█{/}`).join(''))
   rolloutDisplay.render()
@@ -206,16 +161,15 @@ screen.key(['escape', 'q', 'C-c'], async function(ch, key) {
   await ld.close()
   process.exit(0)
 });
+
 async function main() {
   await ld.waitForInitialization();
   const watcher = createWatcher()
   render()
   watcher.on('change', () => {
-    ldLogger.info('example users changed')
     render()
   })
   ld.on('update', () => {
-    ldLogger.info('rules updated')
     render()
   })
 }
